26062dd7720d87914f3f4a18b640ff9d2c027d69
Jorge Eduardo Fermino Oliveira Silva <jorgeubermensch@gmail.com>
2021-12-05 11:03:06 -0300

    Update Game.java

--- 074126_Projeto/src/com/unicamp/mc322/Projeto/Game.java
+++ 074126_Projeto/src/com/unicamp/mc322/Projeto/Game.java
@@ -1,5 +1,8 @@
 package com.unicamp.mc322.Projeto;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.Process;
 
 public class Game {
 	private boolean exitSelected;
@@ -19,27 +22,57 @@ public class Game {
 		
 	}
 	public void readInput() {
-		String command = input.next();
-		command = command.toLowerCase();
+		char command = input.next(".").charAt(0);
+		//input.
+		//command = command.toLowerCase();
 		switch(command){
-			case "quit":
+			case 'q':
+			case 'Q':
 				exitSelected = true;
 				break;
-			case "w":
+			case 'w':
+			case 'W':
 				//moveUp();
 				break;
-			case "s":
+			case 's':
+			case 'S':
 				//moveDown();
 				break;
-			case "a":
+			case 'a':
+			case 'A':
 				//moveLeft();
 				break;
-			case "d":
+			case 'd':
+			case 'D':
 				//moveRight();
+				break;
+			case 'k':
+			case 'K':
+				System.out.println("Teste");
+				break;
+			default:
 				break;
 		}
 	}
 	public void updateBoard() {
 		
+	}
+	//////
+	//Teste
+	/////
+	public static void main(String[] args) {
+		String comando = "echo paada";
+		Game game = new Game();
+		game.readInput();
+		System.out.println("Acabou.");
+		/*String cmd = "ls -al";
+		Runtime run = Runtime.getRuntime();
+		Process pr = run.exec(cmd);
+		pr.waitFor();
+		BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+		String line = "";
+		while ((line=buf.readLine())!=null) {
+		System.out.println(line);*/
+		}
 	}
 }

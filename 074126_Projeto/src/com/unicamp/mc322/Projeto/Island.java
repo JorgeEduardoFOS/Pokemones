0cc749760446cb8df9382c78d387e23ab1251659
Jorge Eduardo Fermino Oliveira Silva <jorgeubermensch@gmail.com>
2021-12-05 10:31:36 -0300

    Update Island.java

--- 074126_Projeto/src/com/unicamp/mc322/Projeto/Island.java
+++ 074126_Projeto/src/com/unicamp/mc322/Projeto/Island.java
@@ -2,8 +2,21 @@ package com.unicamp.mc322.Projeto;
 import java.util.ArrayList;
 
 public class Island {
+	public final static String LIVRE = "--";
+	public final static int VAZIO = 0;
+	public final int MAX = 50;
+	private Integer[][] matrix;
 	private boolean visited;//a dificuldade de captura é configurada todas as vezes que entra na ilha ou só uma vez?
 	private ArrayList<Pokemon> pokemons = new ArrayList<Pokemon>();
+	
+	public Island(int altura, int largura) {
+		matrix = new Integer[altura][largura];
+		for (int i = 0; i < altura; i++) {
+			for ( int j = 0; j < largura; j++) {
+				matrix[i][j]=0;
+			}
+		}		
+	}
 	
 	public void removePokemon(Pokemon pokemon) {
 		pokemons.remove(pokemon);

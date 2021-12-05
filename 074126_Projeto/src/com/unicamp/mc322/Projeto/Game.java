6876fa225d31bde85b45577dc32695ad5117f189
Jorge Eduardo Fermino Oliveira Silva <jorgeubermensch@gmail.com>
2021-12-05 17:37:51 -0300

    Update Game.java

--- 074126_Projeto/src/com/unicamp/mc322/Projeto/Game.java
+++ 074126_Projeto/src/com/unicamp/mc322/Projeto/Game.java
@@ -1,8 +1,5 @@
 package com.unicamp.mc322.Projeto;
 import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.lang.Process;
 
 public class Game {
 	private boolean exitSelected;

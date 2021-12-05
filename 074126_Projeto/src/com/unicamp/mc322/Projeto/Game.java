38b18e5451cb27303d03365abfe240e2b6ae2bef
Jorge Eduardo Fermino Oliveira Silva <jorgeubermensch@gmail.com>
2021-12-05 16:54:50 -0300

    Update Game.java

--- 074126_Projeto/src/com/unicamp/mc322/Projeto/Game.java
+++ 074126_Projeto/src/com/unicamp/mc322/Projeto/Game.java
@@ -1,10 +1,19 @@
 package com.unicamp.mc322.Projeto;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.Process;
 
 public class Game {
 	private boolean exitSelected;
+	private Scanner input2 = new Scanner(System.in);
+	Island ilha;
+	Player player1;
 	
 	public void start() {
 		exitSelected = false;
+		setPlayer();
+		ilha = new Island(player1);
 		System.out.println("Game started!");
 		while(!exitSelected) {
 			drawBoard();
@@ -14,12 +23,66 @@ public class Game {
 		System.out.println("Game termineted. Bye!");
 	}
 	public void drawBoard() {
-		
+		ilha.movePlayer();
+		ilha.printIsland();
 	}
 	public void readInput() {
-		///////Scanner
+		if(player1.getNumberOfMovements() < 1) {
+			player1.rollDices();
+		}
+		else {
+			String command = input2.nextLine();
+			command = command.toLowerCase();
+			switch(command){
+				case "quit":
+					exitSelected = true;
+					break;
+				case "w":
+					if(player1.getCurrentY() > 0) {
+						player1.moveUp();
+						player1.decreasesMovements();
+					}
+					break;
+				case "s":
+					if(player1.getCurrentY() < ilha.getAltura()) {
+						player1.moveDown();
+						player1.decreasesMovements();
+					}
+					break;
+				case "a":
+					if(player1.getCurrentX() > 0) {
+						player1.moveLeft();
+						player1.decreasesMovements();
+					}
+					break;
+				case "d":
+					if(player1.getCurrentX() < ilha.getLargura()) {
+						player1.moveRight();
+						player1.decreasesMovements();
+					}
+					break;
+				case "k":
+					System.out.println("Teste");
+					break;
+				default:
+					break;
+			}
+		}			
 	}
 	public void updateBoard() {
 		
+	}
+	private void setPlayer() {
+		player1 = new Player();
+		player1.setPlayer();
+	}
+		
+	//////
+	//Teste
+	/////
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.readInput();
+		System.out.println("Acabou.");
 	}
 }

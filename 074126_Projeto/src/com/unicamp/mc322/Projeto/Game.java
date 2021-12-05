883ea26c5b12f4ca85b2a7469c02b522a3ff677f
Jorge Eduardo Fermino Oliveira Silva <jorgeubermensch@gmail.com>
2021-12-04 21:39:10 -0300

    Update Game.java input
    começando a mplementar o input

--- 074126_Projeto/src/com/unicamp/mc322/Projeto/Game.java
+++ 074126_Projeto/src/com/unicamp/mc322/Projeto/Game.java
@@ -1,7 +1,9 @@
 package com.unicamp.mc322.Projeto;
+import java.util.Scanner;
 
 public class Game {
 	private boolean exitSelected;
+	private Scanner input = new Scanner(System.in);
 	
 	public void start() {
 		exitSelected = false;
@@ -17,7 +19,25 @@ public class Game {
 		
 	}
 	public void readInput() {
-		///////Scanner
+		String command = input.nextLine();
+		command = command.toLowerCase();
+		switch(command){
+			case "quit":
+				exitSelected = true;
+				break;
+			case "w":
+				//moveUp();
+				break;
+			case "s":
+				//moveDown();
+				break;
+			case "a":
+				//moveLeft();
+				break;
+			case "d":
+				//moveRight();
+				break;
+		}
 	}
 	public void updateBoard() {
 		
